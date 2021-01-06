374130f1bb60cf816d671763e06c779541568b05
oscaremilio <oscaremilio_@hotmail.es>
2021-01-06 13:31:30 +0100

    Cambia el código para que sea correcto

--- beginner/day1-working-with-variables/debugging-practice.py
+++ beginner/day1-working-with-variables/debugging-practice.py
@@ -1,8 +1,9 @@
-# Programa que debe escribir el código siguiente correctamente:
+# Programa con errores en su código.
+# Cambia el código para que sea correcto:
 
 # print(Day 1 - String Manipulation")
 # print("String Concatenation is done with the "+" sign.")
-# print('e.g. print("Hello " + "world")')
+#   print('e.g. print("Hello " + "world")')
 # print(("New lines can be created with a backslash and n.")
 
 print("Day 1 - String Manipulation")
