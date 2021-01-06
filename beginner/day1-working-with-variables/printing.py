ed478d61804dbddc028db860d32f5305ad632fe3
oscaremilio <oscaremilio_@hotmail.es>
2021-01-06 12:29:49 +0100

    Uso de la función print()

---
+++ beginner/day1-working-with-variables/printing.py
@@ -0,0 +1,11 @@
+
+# Programa que escribe por pantalla lo siguiente:
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
