38ca211c2b282d1011d3d279dad2a25c6c0592ff
oscaremilio <oscaremilio_@hotmail.es>
2021-01-06 21:49:01 +0100

    Usa variables para intercambiar valores

---
+++ beginner/day1-working-with-variables/variables.py
@@ -0,0 +1,22 @@
+
+# Programa que intercambia e imprime por pantalla 
+# los valores de las variables a y b.
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
